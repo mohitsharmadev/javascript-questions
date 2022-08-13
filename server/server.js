0b2f33a5ebae2370e5f40d05933eb7eb6450e5eb
Mohit Sharma <ms6015362@gmail.com>
2022-08-13 18:03:41 +0530

    javascript

--- server/server.js
+++ server/server.js
@@ -1,9 +1,50 @@
 const express = require("express");
 const app = express();
+const tasks = [
+  {
+    id: 599,
+    title: "nest",
+    description: "server.js and express.js",
+  },
+  {
+    id: 593,
+    title: "nest",
+    description: "server.js and express.js",
+  },
+  {
+    id: 592,
+    title: "nest",
+    description: "server.js and express.js",
+  },
+  {
+    id: 591,
+    title: "nest",
+    description: "server.js and express.js",
+  },
+];
+app.use(express.urlencoded({ extended: true }));
+
+app.post("", (req, res) => {
+  console.log(req.body);
+  tasks.push({
+    id: req.body.id,
+    title: req.body.title,
+    description: req.body.desc,
+  });
+  res.json(tasks);
+});
 
 app.get("", (req, res) => {
-  res.send("Mohit sharma");
+  res.json(tasks);
 });
-app.listen(3019, () => {
+
+app.get("/:id", (req, res) => {
+  console.log(req.params.id);
+  const result = tasks.find((item) => item.id === Number(req.params.id));
+  console.log(result);
+  res.json(result);
+});
+
+app.listen(3018, () => {
   console.log("server started");
 });
