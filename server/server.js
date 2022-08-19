e3d84bef3534f94337c67758655767a7de5a2d51
Mohit Sharma <ms6015362@gmail.com>
2022-08-19 19:48:04 +0530

    javascript

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const tasks = [
 ];
 app.use(express.urlencoded({ extended: true }));
 
-app.post("", (req, res) => {
+app.post("/", (req, res) => {
   console.log(req.body);
   tasks.push({
     id: req.body.id,
@@ -34,7 +34,7 @@ app.post("", (req, res) => {
   res.json(tasks);
 });
 
-app.get("", (req, res) => {
+app.get("/", (req, res) => {
   res.json(tasks);
 });
 
@@ -43,6 +43,20 @@ app.get("/:id", (req, res) => {
   const result = tasks.find((item) => item.id === Number(req.params.id));
   console.log(result);
   res.json(result);
+});
+app.patch("/:id", (req, res) => {
+  // find index
+  const index = tasks.findIndex((item) => item.id === Number(req.params.id));
+
+  (tasks[index].title = req.body.title),
+    (tasks[index].description = req.body.desc),
+    console.log(index);
+  // get details from body
+  tasks[index].description = "";
+  //update tasks array at index found
+});
+app.put("", (req, res) => {
+  const index = tasks.findIndex((item) => item.id === Number(req.params.id));
 });
 
 app.listen(3018, () => {
